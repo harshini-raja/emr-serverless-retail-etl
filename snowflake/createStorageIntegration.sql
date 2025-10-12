41cfcbabbccfbc6bd7fce028b02d0a499405a1e7
Harshini Raja <63184227+harshini-raja@users.noreply.github.com>
2025-10-12 07:55:06 -0700

    Update createStorageIntegration.sql

--- snowflake/createStorageIntegration.sql
+++ snowflake/createStorageIntegration.sql
@@ -5,8 +5,8 @@ CREATE OR REPLACE STORAGE INTEGRATION s3_instacart_integration
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = S3
   ENABLED = TRUE
-  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::058264291892:role/snowflake-s3-instacart-role'
-  STORAGE_ALLOWED_LOCATIONS = ('s3://instacart-curated-harshini-20251002/');
+  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::<your aws id>:role/snowflake-s3-instacart-role'
+  STORAGE_ALLOWED_LOCATIONS = ('s3://instacart-curated-<your-name>-<date>/');
 
 -- 2) Get the two values you need to build the AWS trust policy
 DESC INTEGRATION s3_instacart_integration;
